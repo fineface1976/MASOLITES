1cfc0599ab5b78f962b2261c8deb881f9dcb5f27
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-23 17:14:40 +0100

    Update token.test.js

--- tests/token.test.js
+++ tests/token.test.js
@@ -1,23 +1,22 @@
+const { expect } = require('chai');  
+const { ethers } = require('hardhat');  
 
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+describe('MASOLToken', () => {  
+    let token, owner, user1;  
 
-describe('MASOLToken', () => {
-    let token, owner, user1;
+    beforeEach(async () => {  
+        [owner, user1] = await ethers.getSigners();  
+        const MASOLToken = await ethers.getContractFactory('MASOLToken');  
+        token = await MASOLToken.deploy();  
+    });  
 
-    beforeEach(async () => {
-        [owner, user1] = await ethers.getSigners();
-        const MASOLToken = await ethers.getContractFactory('MASOLToken');
-        token = await MASOLToken.deploy();
-    });
+    it('Airdrops 50 MSL on registration', async () => {  
+        await token.connect(user1).register(owner.address);  
+        expect(await token.balanceOf(user1.address)).to.equal(50 * 10**18);  
+    });  
 
-    it('Should airdrop 50 MSL on registration', async () => {
-        await token.register(owner.address);
-        expect(await token.balanceOf(user1.address)).to.equal(50 * 10**18);
-    });
-
-    it('Should reward 5% to upline (6 levels)', async () => {
-        await token.register(owner.address); // user1's upline = owner
-        expect(await token.balanceOf(owner.address)).to.equal(10_000_000 * 10**18 - 50 * 10**18 + (50 * 0.05 * 10**18));
-    });
-});
+    it('Rewards 5% to 6 upline levels', async () => {  
+        await token.connect(user1).register(owner.address);  
+        expect(await token.balanceOf(owner.address)).to.equal(10_000_000 * 10**18 - 50 * 10**18 + (50 * 0.05 * 10**18));  
+    });  
+});  
